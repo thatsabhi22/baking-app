f200a5232b8a50c6abc7bc987d98bffb41221d44
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-04-06 08:53:27 +0530

    Current Status: Activities Created, Dish List item layout created Folders created for specific purposes

---
+++ app/src/main/java/com/udacity/bakingapp/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package com.udacity.bakingapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udacity.bakingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
--- app/src/main/java/com/udacity/bakingapp/ui/RecipeActivity.java
+++ app/src/main/java/com/udacity/bakingapp/ui/RecipeActivity.java
@@ -1,16 +1,16 @@
-package com.udacity.bakingapp;
+package com.udacity.bakingapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.udacity.bakingapp.R;
+
+public class RecipeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        
+        setContentView(R.layout.activity_recipe);
     }
 }
