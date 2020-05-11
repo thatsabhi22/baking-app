8a1f571c4fc3434333836b4b9a2689b52c8d3543
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-05-11 21:32:16 +0530

    Made corrections based on the mentor's feedback

--- app/src/androidTest/java/com/udacity/bakingapp/IntentTesting.java
+++ app/src/androidTest/java/com/udacity/bakingapp/IntentTesting.java
@@ -5,8 +5,7 @@ import android.app.Instrumentation;
 
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.runner.AndroidJUnit4;
-
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.udacity.bakingapp.ui.DishesActivity;
 import com.udacity.bakingapp.utils.ConstantsUtil;
 
